a76828cf91616276f766416970ea25f5cb7b3b24
Ponosov Egor <ponosov.qa@gmail.com>
2025-12-11 22:52:54 +0200

    Добавил обработку ошибки если не появиться элимент доработал Xhath для стабильности тестов Добавил функции для перехода к форме востановления пароля

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,4 +1,5 @@
 import allure
+from selenium.common import TimeoutException
 from selenium.webdriver.common.by import By
 from pages.base_page import BasePage
 
@@ -7,7 +8,7 @@ class LoginPageLocators:
     LOGIN_FIELD = (By.ID, "field_email")
     PASSWORD_FIELD = (By.ID, "field_password")
     PASSWORD_TOGGLE_VISIBILITY_BUTTON = (By.CLASS_NAME, "vkuiFormField__iconWrapper")
-    LOGIN_BUTTON = (By.XPATH,"//button[@type='submit' and .//span[text()='Войти']]")
+    LOGIN_BUTTON = (By.XPATH, "//*[@type='submit' and (contains(@class, 'button-pro') or contains(@class, 'vkuiButton__host') or @value='Войти в Одноклассники' or .//*[text()='Войти'])]")
     LOGIN_BUTTON_WITH_QR_CODE = (By.XPATH, "//button[.//span[text() ='Войти по QR-коду']]")
     LOGIN_TAB_QR_CODE = (By.XPATH , '//*[@data-l="t,qr_tab"]')
     REGISTER_BUTTON =  (By.XPATH,"//button[@type='button' and .//span[text()='Зарегистрироваться']]")
@@ -16,17 +17,24 @@ class LoginPageLocators:
     SIGN_IN_YANDEX = ( By.XPATH, '//*[@data-l="t,yandex"]')
     TITLE_ERROR_ON_LOGIN = (By.XPATH , "//span[text()='Введите логин']")
     TITLE_ERROR_ON_PASSWORD = (By.XPATH , "//span[text()='Введите пароль']")
+    BUTTON_RECOVER_ACCOUNT = (By.XPATH, "//span[contains(@class, 'vkuiButton__content') and text()='Восстановить'] | //button[@data-l='t,restore']")
+    GO_BACK_BUTTON = (By.XPATH , "//span[text()='Отмена']")
 
-
-class LoginPageLocatorsHelper(BasePage):
+class LoginPageHelper(BasePage):
     def __init__(self, driver):
         super().__init__(driver)
         self.check_page()
 
     def check_page(self):
+        with allure.step("Проверяем корректность загрузки страницы"):
+            self.attach_screenshot()
         self.find_element(LoginPageLocators.LOGIN_FIELD)
         self.find_element(LoginPageLocators.PASSWORD_FIELD)
-        self.find_element(LoginPageLocators.PASSWORD_TOGGLE_VISIBILITY_BUTTON)
+        try:
+            self.find_element(LoginPageLocators.PASSWORD_TOGGLE_VISIBILITY_BUTTON)
+            return True
+        except TimeoutException:
+            return False
         self.find_element(LoginPageLocators.LOGIN_BUTTON)
         self.find_element(LoginPageLocators.LOGIN_BUTTON_WITH_QR_CODE)
         self.find_element(LoginPageLocators.REGISTER_BUTTON)
@@ -37,7 +45,7 @@ class LoginPageLocatorsHelper(BasePage):
     @allure.step("Нажимаем на кнопку 'Войти'")
     def click_login(self):
         self.attach_screenshot()
-        self.find_element(LoginPageLocators.LOGIN_BUTTON).click()
+        self.find_element(locator=LoginPageLocators.LOGIN_BUTTON, time=2).click()
 
     @allure.step("Получаем текст ошибки 'Введите логин'")
     def get_error_login(self):
@@ -49,7 +57,18 @@ class LoginPageLocatorsHelper(BasePage):
         self.attach_screenshot()
         return  self.find_element(LoginPageLocators.TITLE_ERROR_ON_PASSWORD).text
 
-    @allure.step("Заполняем поле Логин")
+    @allure.step("Заполняем поле логин")
     def send_login(self, text):
         self.attach_screenshot()
         return  self.find_element(LoginPageLocators.LOGIN_FIELD).send_keys(text)
+
+    @allure.step("Заполняем поле пароль")
+    def send_password(self, password):
+        self.attach_screenshot()
+        return self.find_element(LoginPageLocators.PASSWORD_FIELD).send_keys(password)
+
+    @allure.step("Переходим к восстановлению")
+    def click_recovery(self):
+        self.attach_screenshot()
+        return self.find_element(LoginPageLocators.BUTTON_RECOVER_ACCOUNT).click()
+
